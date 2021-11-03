ffe8e5e57dda9ff6bca316ba0a5588b0761b0ddc
wangkunlun <wangkunlun@taou.com>
2021-11-03 11:13:42 +0800

    feat: JD

--- example/skeleton_vue/skeleton.config.js
+++ example/skeleton_vue/skeleton.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     url: 'https://www.baidu.com',
+    // url: 'http://localhost:3000/jobs/job_detail?fr=search_job&ejid=Ksy3RDNCNhyHT80KuTg71Q',
     output: {
         // filepath: '/Users/maimai/Desktop/maimai/React/skeleton/public/index.html',
         filepath: 'index.js',
@@ -7,12 +8,12 @@ module.exports = {
         fileDir: 'vue-skeleton',
         injectSelector: 'skeleton'
     },
-    pageName: 'baidu-pc',
+    pageName: 'baidu',
     sleepTime: 500,
     loadDestory: false,
     device: 'mobile',
     extraHTTPHeaders: {
-        cookie: '_buuid=e24c250e-b4c9-4711-ba83-1e360e3107de; Hm_lvt_8724ff68e94c54871ef930a43c375b9d=1635316808; csrftoken=WcUlKTbx-5bnXBS91XSioIREqdDCBsCl5bMM; Hm_lvt_125466a4193d3041720419bb940c71b9=1634897722,1635316845,1635414554,1635734362; seid=s1635840657346; guid=HBsEHxoEGxscBBhWBxgbGRMbGB0dGxtWHBkEHRkfBUNYS0xLeQoeHwQSGRweBBoEHxMFT0dFWEJpCgNFQUlPbQpPQUNGCgZmZ35iYQIKHBkEHRkfBV5DYUhPfU9GWlprCgMddR8bdRobCnIKeWUKSUtnCkZPXkRjChFCWUVeRENJS2cCChoEHwVLRkZDUEVn; access_token=1.a6039c8b86f18569b3508a437e9f0192; u=171759974; maimai_version=4.0.0; channel=www; session=eyJzZWNyZXQiOiJlU05USDgxNEtoNmpuMlFtbXRrcE1RT00iLCJzdGF0dXMiOnRydWUsInUiOiIxNzE3NTk5NzQiLCJfZXhwaXJlIjoxNjM1OTI4MjIxMjY1LCJfbWF4QWdlIjo4NjQwMDAwMH0=; session.sig=-25n26NWA7AW2wPFGSeWdfYZx_Y; Hm_lpvt_125466a4193d3041720419bb940c71b9=1635841828'
+        cookie: '_buuid=9295bd7d-914c-4d47-9637-b7810d43b38d; csrftoken=fjfhlnOT-qf_jfBN7I-LpOqtb5ZDlHQgeG4Q; Hm_lvt_125466a4193d3041720419bb940c71b9=1633658633,1635492017,1635906982; Hm_lpvt_125466a4193d3041720419bb940c71b9=1635907908; session=eyJzZWNyZXQiOiJnUWxFT1RJM3diS1VlNkxDMGo5WXppTU8iLCJ1IjotMSwiX2V4cGlyZSI6MTYzNTk5NDMwODY1MywiX21heEFnZSI6ODY0MDAwMDB9; session.sig=A3idK9IQnGnTRpyGTxSYf48lAIE'
     },
     pageShowContain: ['mobile'], // mobile, pc
 }
